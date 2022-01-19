f8b644c04056b6380fe8eaa33caced403e9ba9ec
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-01-19 14:09:52 +0600

    bubbleSort

---
+++ BubbleSort/bubbleSort.java
@@ -0,0 +1,30 @@
+package com.sourav.Problem_Solving.BubbleSort;
+
+import java.util.Arrays;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[]arr={4,5,6,2,0,1,7};
+        bubble(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[]arr){
+        boolean swaped;
+        for (int i=0;i<arr.length;i++){
+            swaped=false;
+            for(int j=1;j<arr.length-i;j++){
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swaped=true;
+                }
+            }
+            if(!swaped){
+                break;
+            }
+        }
+
+    }
+}
