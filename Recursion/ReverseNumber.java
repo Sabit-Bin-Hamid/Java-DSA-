d5a6fea49e735dcb57d1133116a4055f96977297
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-01-20 23:30:47 +0600

    ReverseNumber

--- Recursion/ReverseNumber.java
+++ Recursion/ReverseNumber.java
@@ -18,7 +18,7 @@ public class ReverseNumber {
     static int helper(int n ,int digit){
         if(n%10==n){
             return n;
-        }
+        }//
         int rem=n%10;
         return rem * (int)(Math.pow(10,digit-1)) + helper(n/10,digit-1);
     }
