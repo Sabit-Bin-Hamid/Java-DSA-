34486381f135322689609783e03bf42d88917cc8
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-01-20 23:26:53 +0600

    bubblesort

--- BubbleSort/bubbleSort.java
+++ BubbleSort/bubbleSort.java
@@ -6,7 +6,7 @@ public class bubbleSort {
     public static void main(String[] args) {
         int[]arr={4,5,6,2,0,1,7};
         bubble(arr);
-
+        //
         System.out.println(Arrays.toString(arr));
         System.out.println(sum(arr));
     }
