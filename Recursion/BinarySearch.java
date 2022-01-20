809a0cda651934861e977e952cfdf2b5b1dd270c
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-01-20 23:29:07 +0600

    BinarySearch

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -17,6 +17,7 @@ public class BinarySearch {
         if(target < arr[mid]){
             return search(arr,target,s,mid-1);
         }
+        //
         return search(arr,target,mid+1,e);
     }
 
