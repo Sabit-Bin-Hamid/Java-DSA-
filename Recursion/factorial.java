89b3f2daadc0da62408eb108d4e7f47239563b8e
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-01-20 23:29:59 +0600

    factorial

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -4,7 +4,7 @@ public class factorial {
     public static void main(String[] args) {
         int n=0;
         System.out.println(fac(n));
-    }
+    }//
     static int fac(int n){
         if(n<=1){
             return 1;
