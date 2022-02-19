803ae91f44c461da8506b0f3f6f8b84394bde2db
Sabit-Bin-Hamid <74405314+Sabit-Bin-Hamid@users.noreply.github.com>
2022-02-19 22:25:10 +0600

    SubSetOfArray

---
+++ Recursion/Recursion_1/SubSetOfArray.java
@@ -0,0 +1,27 @@
+package com.sourav.Problem_Solving.Recursion.Recursion_1;
+
+import java.util.*;
+
+public class SubSetOfArray {
+    public static void main(String[] args) {
+        int[]a={1,2,3};
+        List<List<Integer>> ans=subset(a);
+        for (List<Integer> list : ans){
+            System.out.println(list);
+        }
+    }
+    static List<List<Integer>> subset(int[]arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for(int num : arr){
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> inner = new ArrayList<>(outer.get(i));
+                inner.add(num);
+                outer.add(inner);
+            }
+        }
+        return outer;
+    }
+}
